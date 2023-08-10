641bdb763b0b3aec94dde6dc5cda07ea57013493
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-10 22:15:30 +0530

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,80 @@
 import "./App.scss"
 import Task from "./Task"
+import { useReducer, useState } from "react"
+
+// ACTIONS
+export const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo"
+}
+
+
+
+
 
 const App = () => {
+
+  // Reducer
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case ACTIONS.ADD_TODO:
+        return [...state, { id: state.length + 1, name: action.payload.name }];
+
+
+      case ACTIONS.TOGGLE_TODO:
+        return state.map((state) => {
+          if (state.id === action.payload.id) {
+            return { ...state, complete: !state.complete }
+          }
+          return state;
+        })
+      case ACTIONS.DELETE_TODO:
+        return state.filter((state) => state.id !== action.payload.id);
+
+      default:
+        return state;
+    }
+  }
+  // Initial State
+  const initialState = [];
+  //useReducer
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //handleSubmit
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "") return;
+    dispatch({
+      type: ACTIONS.ADD_TODO,
+      payload: { name }
+    })
+    setName("");
+
+  }
+  // Name
+  const [name, setName] = useState("");
+
+  console.log(state)
+
   return (
     <div className="App">
       <div className="Title">
         <h1>Todo App</h1>
       </div>
-      <div className="AddTask">
-        <input type="text" placeholder="Add a new task.." />
-        <button>Add</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="AddTask">
+          <input type="text" placeholder="Add a new task.." value={name} onChange={(e) => setName(e.target.value)} />
+          <button>Add</button>
+        </div>
+      </form>
       <div className="Tasks">
-        <Task />
-        <Task />
-        <Task />
+        {state.map((Obj) => {
+          return (
+            <Task key={Obj.id} Task={Obj} dispatch={dispatch} />
+          )
+        })}
       </div>
     </div>
   )
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,15 +1,16 @@
 import "./App.scss"
+import { ACTIONS } from "./App"
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-const Task = () => {
+const Task = ({ Task, dispatch }) => {
     return (
         <div className="Task">
-            <div className="Task-Text">
-                <p>Task 1</p>
+            <div className={(Task.complete?"Task-Text Completed" : "Task-Text")}>
+                <p>{Task.name}</p>
             </div>
             <div className="Task-Buttons">
-                <button >Done</button>
-                <div className="Task-Delete" >
+                <button onClick={() => dispatch({type: ACTIONS.TOGGLE_TODO, payload:{id: Task.id}})}>{Task.complete? "Ongoing": "Done"}</button>
+                <div className="Task-Delete" onClick={() => dispatch({type: ACTIONS.DELETE_TODO, payload: {id: Task.id}})} >
                     <DeleteForeverIcon />
                 </div>
             </div>
