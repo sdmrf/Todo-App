1d30feb128fc30b970b34b654d3bb00c170a4624
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-10 15:02:23 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,11 @@ const App = () => {
         <h1>Todo App</h1>
       </div>
       <div className="AddTask">
-        <input type="text" placeholder="Add a new task" />
+        <input type="text" placeholder="Add a new task.." />
         <button>Add</button>
       </div>
-      
-      <div className="Tasks">
 
-        <ul>
-          <li>
-            <span>Task 1</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-          <li>
-            <span>Task 2</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-        </ul>
+      <div className="Tasks">
       </div>
 
     </div>
