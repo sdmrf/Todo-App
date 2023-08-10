a43868c2e6b6bf2d3b0643134a806a5329af8279
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-10 14:49:32 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="Title">
+        <h1>Todo App</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="AddTask">
+        <input type="text" placeholder="Add a new task" />
+        <button>Add</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <div className="Tasks">
+
+        <ul>
+          <li>
+            <span>Task 1</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+          <li>
+            <span>Task 2</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
