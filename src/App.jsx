b7b8219de2895b2f8de2eac9618dd075568e07f2
Syed Maruf Ali <syedmarufali23@gmail.com>
2023-08-13 15:23:54 +0530

    Local Storage Feature Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,61 @@
-import "./App.scss"
-import Task from "./Task"
-import { useReducer, useState } from "react"
+import "./App.scss";
+import Task from "./Task";
+import { useReducer, useState, useEffect } from "react";
 
 // ACTIONS
 export const ACTIONS = {
   ADD_TODO: "add-todo",
   TOGGLE_TODO: "toggle-todo",
   DELETE_TODO: "delete-todo"
-}
-
-
-
-
+};
 
 const App = () => {
-
   // Reducer
   const reducer = (state, action) => {
     switch (action.type) {
       case ACTIONS.ADD_TODO:
-        return [...state, { id: state.length + 1, name: action.payload.name }];
-
+        return [...state, { id: state.length + 1, name: action.payload.name, complete: false }];
 
       case ACTIONS.TOGGLE_TODO:
-        return state.map((state) => {
-          if (state.id === action.payload.id) {
-            return { ...state, complete: !state.complete }
+        return state.map((todo) => {
+          if (todo.id === action.payload.id) {
+            return { ...todo, complete: !todo.complete };
           }
-          return state;
-        })
+          return todo;
+        });
+
       case ACTIONS.DELETE_TODO:
-        return state.filter((state) => state.id !== action.payload.id);
+        return state.filter((todo) => todo.id !== action.payload.id);
 
       default:
         return state;
     }
-  }
-  // Initial State
-  const initialState = [];
-  //useReducer
+  };
+
+  // Load todos from local storage on initial render
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  //handleSubmit
+  // Save todos to local storage whenever they change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(state));
+  }, [state]);
 
+  // handleSubmit
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name === "") return;
     dispatch({
       type: ACTIONS.ADD_TODO,
       payload: { name }
-    })
+    });
     setName("");
+  };
 
-  }
   // Name
   const [name, setName] = useState("");
 
-  console.log(state)
+  console.log(state);
 
   return (
     <div className="App">
@@ -70,14 +69,12 @@ const App = () => {
         </div>
       </form>
       <div className="Tasks">
-        {state.map((Obj) => {
-          return (
-            <Task key={Obj.id} Task={Obj} dispatch={dispatch} />
-          )
+        {state.map((todo) => {
+          return <Task key={todo.id} Task={todo} dispatch={dispatch} />;
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
